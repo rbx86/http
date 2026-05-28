39ee2b374552774605e05973d70b84ec06f959f7
rbx86 <210249546+rbx86@users.noreply.github.com>
2026-05-29 00:51:43 +0530

    format

--- server.c
+++ server.c
@@ -1,12 +1,12 @@
+#include <arpa/inet.h>
+#include <err.h>
+#include <netdb.h>
 #include <netinet/in.h>
 #include <stddef.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <string.h>
 #include <stdio.h>
-#include <err.h>
+#include <string.h>
+#include <sys/socket.h>
 #include <unistd.h>
-#include <arpa/inet.h>
 
 #define BACKLOGS 10
 #define BUFF_SIZE 1024
@@ -17,26 +17,22 @@ int main() {
   struct sockaddr_storage client_addr;
   int sockfd, client_fd;
   char buf[BUFF_SIZE];
-  
+
   memset(&hints, 0, sizeof(hints));
 
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_flags = AI_PASSIVE;
 
-  int status =  getaddrinfo(NULL, "3490", &hints, &res);
+  int status = getaddrinfo(NULL, "3490", &hints, &res);
   if (status != 0) {
     fprintf(stderr, "%s\n", gai_strerror(status));
     return 1;
   }
   printf("[*] Get Address Info Successful\n");
 
-  sockfd = socket(
-    res->ai_family,
-    res->ai_socktype,
-    res->ai_protocol
-  );
-  if (sockfd == -1){
+  sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+  if (sockfd == -1) {
     perror("server: socket");
     freeaddrinfo(res);
     return 1;
@@ -50,7 +46,7 @@ int main() {
     return 1;
   }
   printf("[*] Bind Successful\n");
-  
+
   freeaddrinfo(res);
 
   if (listen(sockfd, BACKLOGS) == -1) {
@@ -61,7 +57,7 @@ int main() {
   printf("[*] Listening on port 3490\n");
 
   socklen_t addr_len = sizeof(client_addr);
-  while(1) {
+  while (1) {
     client_fd = accept(sockfd, (struct sockaddr *)&client_addr, &addr_len);
 
     if (client_addr.ss_family == AF_INET) {
@@ -87,10 +83,11 @@ int main() {
     printf("[/] Received Buffer:\n");
     printf("%s\n", buf);
 
-    char *msg = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<h1>hello, world</h1>\n";
+    char *msg = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<h1>hello, "
+                "world</h1>\n";
     size_t msg_size = strlen(msg);
     int bytes_sent = send(client_fd, msg, msg_size, 0);
-    if (bytes_sent == -1){
+    if (bytes_sent == -1) {
       perror("server: send");
       close(client_fd);
       close(sockfd);
