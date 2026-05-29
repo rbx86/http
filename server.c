f0063063a06911d563d289b543e3c701d7414afa
rbx86 <210249546+rbx86@users.noreply.github.com>
2026-05-29 15:29:34 +0530

    code clean up

--- server.c
+++ server.c
@@ -8,25 +8,65 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+#define PORT "3490"
 #define BACKLOGS 10
 #define BUFF_SIZE 1024
+
+void handle_client(int client_fd, struct sockaddr_storage *client_addr) {
+
+  char buf[BUFF_SIZE];
+
+  if (client_addr->ss_family == AF_INET) {
+    char ip4[INET_ADDRSTRLEN];
+    struct sockaddr_in *client_in = (struct sockaddr_in *)client_addr;
+    inet_ntop(AF_INET, &(client_in->sin_addr), ip4, INET_ADDRSTRLEN);
+    printf("Client IPv4 Address: %s\n", ip4);
+  } else {
+    char ip6[INET6_ADDRSTRLEN];
+    struct sockaddr_in6 *client_in6 = (struct sockaddr_in6 *)client_addr;
+    inet_ntop(AF_INET6, &(client_in6->sin6_addr), ip6, INET6_ADDRSTRLEN);
+    printf("Client IPv6 Address: %s\n", ip6);
+  }
+
+  ssize_t bytes_recvd = recv(client_fd, buf, BUFF_SIZE - 1, 0);
+  if (bytes_recvd == -1) {
+    perror("server: recv");
+    goto end_conn;
+  }
+  buf[bytes_recvd] = '\0';
+  printf("[/] Recieved Buffer:\n%s\n", buf);
+
+  char *msg = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<h1>hello, "
+                "world</h1>\n";
+  size_t msg_size = strlen(msg);
+
+  ssize_t bytes_sent = send(client_fd, msg, msg_size, 0);
+  if (bytes_sent == -1) {
+    perror("server: send");
+    goto end_conn;
+  }
+  
+  printf("[/] Message Sent Successfully\n");
+    
+  end_conn:
+    printf("[*] Terminating Connection...\n");
+    close(client_fd);
+}
 
 int main() {
 
   struct addrinfo hints, *res;
   struct sockaddr_storage client_addr;
   int sockfd, client_fd;
-  char buf[BUFF_SIZE];
 
   memset(&hints, 0, sizeof(hints));
-
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_flags = AI_PASSIVE;
 
-  int status = getaddrinfo(NULL, "3490", &hints, &res);
-  if (status != 0) {
-    fprintf(stderr, "%s\n", gai_strerror(status));
+  int gai = getaddrinfo(NULL, PORT, &hints, &res);
+  if (gai != 0) {
+    fprintf(stderr, "%s\n", gai_strerror(gai));
     return 1;
   }
   printf("[*] Get Address Info Successful\n");
@@ -56,47 +96,18 @@ int main() {
   }
   printf("[*] Listening on port 3490\n");
 
-  socklen_t addr_len = sizeof(client_addr);
   while (1) {
+    socklen_t addr_len = sizeof(client_addr);
     client_fd = accept(sockfd, (struct sockaddr *)&client_addr, &addr_len);
 
-    if (client_addr.ss_family == AF_INET) {
-      char ip4[INET_ADDRSTRLEN];
-      struct sockaddr_in *client_in = (struct sockaddr_in *)&client_addr;
-      inet_ntop(AF_INET, &(client_in->sin_addr), ip4, INET_ADDRSTRLEN);
-      printf("Client IPv4 Address: %s\n", ip4);
-    } else {
-      char ip6[INET6_ADDRSTRLEN];
-      struct sockaddr_in6 *client_in6 = (struct sockaddr_in6 *)&client_addr;
-      inet_ntop(AF_INET6, &(client_in6->sin6_addr), ip6, INET6_ADDRSTRLEN);
-      printf("Client IPv6 Address: %s\n", ip6);
+    if (client_fd == -1) {
+      perror("server: accept");
+      continue;
     }
 
-    int bytes_recvd = recv(client_fd, buf, BUFF_SIZE, 0);
-    if (bytes_recvd == -1) {
-      perror("server: recv");
-      close(client_fd);
-      close(sockfd);
-      return 1;
-    }
-    buf[bytes_recvd] = '\0';
-    printf("[/] Received Buffer:\n");
-    printf("%s\n", buf);
-
-    char *msg = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<h1>hello, "
-                "world</h1>\n";
-    size_t msg_size = strlen(msg);
-    int bytes_sent = send(client_fd, msg, msg_size, 0);
-    if (bytes_sent == -1) {
-      perror("server: send");
-      close(client_fd);
-      close(sockfd);
-      return 1;
-    }
-    printf("[/] Message Sent Successfully\n");
-    printf("[*] Terminating Connection...\n");
-    close(client_fd);
+    handle_client(client_fd, &client_addr);
   }
+  
   close(sockfd);
   return 0;
 }
